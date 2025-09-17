3b0f42a76bd6a359ce4121765755e5a755cc8b9f
Arunkumar Saravanan <41850738+arunkumarucet@users.noreply.github.com>
2025-09-17 11:32:39 +0530

    updates

--- pinot/run.sh
+++ pinot/run.sh
@@ -6,7 +6,7 @@ cat queries.sql | while read -r query; do
     echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
     echo -n "["
     for i in $(seq 1 $TRIES); do
-        echo "{\"sql\":\"$query  option(timeoutMs=300000)\"}"| tr -d ';' > query.json
+        jq -n --arg sql "$query" --arg opts "timeoutMs=300000" '{"sql": $sql, "queryOptions": $opts}' > query.json
         RES=$(curl -s -XPOST -H'Content-Type: application/json' http://localhost:8000/query/sql/ -d @query.json | jq 'if .exceptions == [] then .timeUsedMs/1000 else "-" end' )
         [[ "$?" == "0" ]] && echo -n "${RES}" || echo -n "null"
         [[ "$i" != $TRIES ]] && echo -n ", "
